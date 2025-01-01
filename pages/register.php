44ead08d8e999e0c3d9448a2e94365fae662e67f
Sushil Kumar <sushilkumbhar77@gmail.com>
2025-01-01 22:57:34 +0530

    A Complate Login System

---
+++ pages/register.php
@@ -0,0 +1,88 @@
+<?php include_once("./header.php"); ?>
+<div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-md-8 col-lg-6">
+                        <div class="card shadow">
+                            <div class="card-body">
+                                <h1
+                                    class="card-title text-center text-uppercase"
+                                >
+                                    Register
+                                </h1>
+                                <form class="card-text">
+                                    <div class="mb-3">
+                                        <input
+                                            type="hidden"
+                                            name="csrf_token"
+                                            value=""
+                                        />
+                                    </div>
+                                    <div class="mb-3">
+                                        <label
+                                            for="exampleInputEmail1"
+                                            class="form-label"
+                                            >Email address</label
+                                        >
+                                        <input
+                                            type="email"
+                                            name="email"
+                                            class="form-control"
+                                            id="exampleInputEmail1"
+                                            placeholder="Email"
+                                            value=""
+                                            required
+                                        />
+                                    </div>
+                                    <div class="mb-3">
+                                        <label
+                                            for="exampleInputPassword1"
+                                            class="form-label"
+                                            >Password</label
+                                        >
+                                        <input
+                                            type="password"
+                                            class="form-control"
+                                            id="exampleInputPassword1"
+                                            name="password"
+                                            minlength="8"
+                                            placeholder="Password"
+                                            value=""
+                                            required
+                                        />
+                                    </div>
+                                    <div class="mb-3 form-check">
+                                        <input
+                                            type="checkbox"
+                                            class="form-check-input"
+                                            id="exampleCheck1"
+                                            name="remember"
+                                        />
+                                        <label
+                                            class="form-check-label"
+                                            for="exampleCheck1"
+                                            >Remember me</label
+                                        >
+                                    </div>
+                                    <div
+                                        class="d-flex align-items-center justify-content-between"
+                                    >
+                                        <button
+                                            type="submit"
+                                            class="btn btn-primary"
+                                        >
+                                            Submit
+                                        </button>
+                                        <span
+                                            >Login
+                                            <a href="./login.php"
+                                                >here?</a
+                                            ></span
+                                        >
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+<?php include_once("./footer.php");?>
