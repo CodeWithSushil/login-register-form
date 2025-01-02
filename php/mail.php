72b5de6f91866ef1883f02a4196c209318941c00
Sushil Kumar <sushilkumbhar77@gmail.com>
2025-01-02 15:08:26 +0530

    Update

--- php/mail.php
+++ php/mail.php
@@ -6,8 +6,10 @@ use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
 //Load Composer's autoloader
-require '../vendor/autoload.php';
+require './vendor/autoload.php';
 
+$dotenv = Dotenv\Dotenv::createImmutable("./");
+$dotenv->safeLoad();
 //Create an instance; passing `true` enables exceptions
 $mail = new PHPMailer(true);
 
@@ -15,24 +17,24 @@ try {
     //Server settings
     $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
     $mail->isSMTP();                                            //Send using SMTP
-    $mail->Host       = 'smtp.example.com';                     //Set the SMTP server to send through
+    $mail->Host       = $_ENV['MAIL_HOST'];// getenv('MAIL_HOST');                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = 'user@example.com';                     //SMTP username
-    $mail->Password   = 'secret';                               //SMTP password
+    $mail->Username   = $_ENV['MAIL_USER'];                     //SMTP username
+    $mail->Password   = $_ENV['MAIL_PSWD'];                               //SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
     $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
     //Recipients
-    $mail->setFrom('from@example.com', 'Mailer');
-    $mail->addAddress('joe@example.net', 'Joe User');     //Add a recipient
-    $mail->addAddress('ellen@example.com');               //Name is optional
-    $mail->addReplyTo('info@example.com', 'Information');
-    $mail->addCC('cc@example.com');
-    $mail->addBCC('bcc@example.com');
+    $mail->setFrom('J68490628@gmail.com', 'Mailer');
+    $mail->addAddress('J68490628@gmail.com', 'Joe User');     //Add a recipient
+  //  $mail->addAddress('ellen@example.com');               //Name is optional
+  //  $mail->addReplyTo('info@example.com', 'Information');
+ //   $mail->addCC('cc@example.com');
+ //   $mail->addBCC('bcc@example.com');
 
     //Attachments
-    $mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
-    $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
+    //$mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
+   // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
 
     //Content
     $mail->isHTML(true);                                  //Set email format to HTML
